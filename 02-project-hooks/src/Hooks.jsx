f7f02f6a6b6026f817a8a77397a5e316b90492d0
OsorioJc-696 <e_2019200661k@uncp.edu.pe>
2023-12-14 18:26:07 -0500

    first commit

--- 02-project-hooks/src/Hooks.jsx
+++ 02-project-hooks/src/Hooks.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Hooks = () => {
+  const [count, setCount] = useState(0);
+  const ref=useRef();
+  // useEffect(() => {
+  //   console.log(`Hiciste click ${count} veces`)
+  // });
+  useEffect(() => {
+    console.log('Me ejecuto al renederizar')
+  }, []);
+  // useEffect(() => {
+  //   console.log('Me ejecuto porque count esta variando')
+  // }, [count]);
   return (
     <div className="bg-ligth min-vh-100">
       <div className="d-flex flex-column align-items-center justify-content-center py-5">
         <img src="https://breakingbadquotes.xyz/img/logo.png" alt="" />
-        <h1 className="">Breaking Bad API</h1>
-        <button className="btn btn-success">Recargar</button>
+        <h1 className="">Breaking Bad APIs</h1>
+        <button className="btn btn-success" onClick={()=>setCount(count+1)}>Recargar</button>
+        <h4 ref={ref} onClick={()=>ref.current.style.color='blue'}>{count}</h4>
       </div>
       <div className="d-flex flex-column align-items-center justify-content-center px-3">
         <blockquote className="border-left pl-2 text-center" style={{ 
